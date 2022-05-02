6a9b91d247caa0efe22caded587f40ca18e246da
kurtzkaranq <mendbayarerxes@gmail.com>
2022-05-02 12:51:14 +0800

    iniital

---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function Test() {
+  return (
+    <div>
+      <h1>This is test</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dicta
+        maxime eius quis voluptatum saepe facilis similique neque sit
+        distinctio, enim excepturi quos voluptatibus, exercitationem rem eos!
+        Dicta, temporibus optio?
+      </p>
+    </div>
+  );
+}
+
+export default Test;
